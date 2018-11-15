df44833bd21b556e74785774e7c1f0d4ae7ebb65
Bianca Nenciu <nenciu.bianca@gmail.com>
2018-11-15 12:00:20 +0200

    FIX: Added onceoff job to migrate to new name.

---
+++ app/jobs/onceoff/rename_canned_replies.rb
@@ -0,0 +1,11 @@
+module Jobs
+  class RenameCannedReplies < Jobs::Onceoff
+    OLD_PLUGIN_NAME = "canned_replies"
+    NEW_PLUGIN_NAME = "discourse-canned-replies"
+
+    def execute_onceoff(args)
+      PluginStoreRow.where(plugin_name: NEW_PLUGIN_NAME).delete_all
+      PluginStoreRow.where(plugin_name: OLD_PLUGIN_NAME).update_all(plugin_name: NEW_PLUGIN_NAME)
+    end
+  end
+end
