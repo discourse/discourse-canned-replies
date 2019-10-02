5ca6363233a4ef00da45a281e4263ec1740ea9c8
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2019-10-02 14:30:58 +1000

    FIX: Use top-level namespace for base classes (#51)

--- app/jobs/onceoff/rename_canned_replies.rb
+++ app/jobs/onceoff/rename_canned_replies.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Jobs
-  class RenameCannedReplies < Jobs::Onceoff
+  class RenameCannedReplies < ::Jobs::Onceoff
     OLD_PLUGIN_NAME = "canned_replies"
     NEW_PLUGIN_NAME = "discourse-canned-replies"
 
