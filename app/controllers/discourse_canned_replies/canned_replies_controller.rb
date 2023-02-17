d04ec80b2fc0ea00e26ec2cfa21f3954c4616f5e
Bianca Nenciu <nenciu.bianca@gmail.com>
2023-02-17 18:41:14 +0200

    DEV: Update plugin to match latest guidelines
    - Define extension modules
    - Use different files instead of plugin.rb
    - Make sure plugin is disabled according to the setting

---
+++ app/controllers/discourse_canned_replies/canned_replies_controller.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+module DiscourseCannedReplies
+  class CannedRepliesController < ::ApplicationController
+    requires_plugin DiscourseCannedReplies::PLUGIN_NAME
+    before_action :ensure_logged_in
+    before_action :ensure_canned_replies_enabled
+    skip_before_action :check_xhr
+
+    def index
+      user_id = current_user.id
+      replies = DiscourseCannedReplies::Reply.all(user_id)
+
+      render json: { replies: replies }
+    end
+
+    def create
+      guardian.ensure_can_edit_canned_replies!
+
+      title = params.require(:title)
+      content = params.require(:content)
+      user_id = current_user.id
+
+      record = DiscourseCannedReplies::Reply.add(user_id, title, content)
+      render json: record
+    end
+
+    def update
+      guardian.ensure_can_edit_canned_replies!
+
+      reply_id = params.require(:id)
+      title = params.require(:title)
+      content = params.require(:content)
+      user_id = current_user.id
+
+      record = DiscourseCannedReplies::Reply.edit(user_id, reply_id, title, content)
+      render json: record
+    end
+
+    def destroy
+      guardian.ensure_can_edit_canned_replies!
+
+      reply_id = params.require(:id)
+      user_id = current_user.id
+      record = DiscourseCannedReplies::Reply.remove(user_id, reply_id)
+
+      render json: record
+    end
+
+    def reply
+      reply_id = params.require(:id)
+      user_id = current_user.id
+
+      record = DiscourseCannedReplies::Reply.get_reply(user_id, reply_id)
+      render json: record
+    end
+
+    def use
+      reply_id = params.require(:id)
+      user_id = current_user.id
+      record = DiscourseCannedReplies::Reply.use(user_id, reply_id)
+
+      render json: record
+    end
+
+    private
+
+    def ensure_canned_replies_enabled
+      guardian.ensure_can_use_canned_replies!
+    end
+  end
+end
--- plugin.rb
+++ plugin.rb
@@ -14,151 +14,31 @@ register_asset "stylesheets/canned-replies.scss"
 register_svg_icon "far-clipboard" if respond_to?(:register_svg_icon)
 
 after_initialize do
-  load File.expand_path("../app/jobs/onceoff/rename_canned_replies.rb", __FILE__)
-
-  module ::CannedReply
-    PLUGIN_NAME ||= "discourse-canned-replies".freeze
-    STORE_NAME ||= "replies".freeze
+  module ::DiscourseCannedReplies
+    PLUGIN_NAME = "discourse-canned-replies"
+    STORE_NAME = "replies"
 
     class Engine < ::Rails::Engine
-      engine_name CannedReply::PLUGIN_NAME
-      isolate_namespace CannedReply
+      engine_name DiscourseCannedReplies::PLUGIN_NAME
+      isolate_namespace DiscourseCannedReplies
     end
   end
 
-  class CannedReply::Reply
-    class << self
-      def add(user_id, title, content)
-        id = SecureRandom.hex(16)
-        record = { id: id, title: title, content: content }
+  require_relative "app/controllers/discourse_canned_replies/canned_replies_controller.rb"
+  require_relative "app/jobs/onceoff/rename_canned_replies.rb"
+  require_relative "app/models/discourse_canned_replies/reply.rb"
 
-        replies = PluginStore.get(CannedReply::PLUGIN_NAME, CannedReply::STORE_NAME) || {}
-
-        replies[id] = record
-        PluginStore.set(CannedReply::PLUGIN_NAME, CannedReply::STORE_NAME, replies)
-
-        record
-      end
-
-      def edit(user_id, reply_id, title, content)
-        record = { id: reply_id, title: title, content: content }
-        remove(user_id, reply_id)
-
-        replies = PluginStore.get(CannedReply::PLUGIN_NAME, CannedReply::STORE_NAME) || {}
-
-        replies[reply_id] = record
-        PluginStore.set(CannedReply::PLUGIN_NAME, CannedReply::STORE_NAME, replies)
-
-        record
-      end
-
-      def all(user_id)
-        replies = PluginStore.get(CannedReply::PLUGIN_NAME, CannedReply::STORE_NAME)
-
-        if replies.blank?
-          add_default_reply
-          replies = PluginStore.get(CannedReply::PLUGIN_NAME, CannedReply::STORE_NAME)
-        end
-
-        return [] if replies.blank?
-        replies.values.sort_by { |reply| reply["title"] || "" }
-      end
-
-      def get_reply(user_id, reply_id)
-        replies = all(user_id)
-
-        replies.detect { |reply| reply["id"] == reply_id }
-      end
-
-      def remove(user_id, reply_id)
-        replies = PluginStore.get(CannedReply::PLUGIN_NAME, CannedReply::STORE_NAME)
-        replies.delete(reply_id)
-        PluginStore.set(CannedReply::PLUGIN_NAME, CannedReply::STORE_NAME, replies)
-      end
-
-      def use(user_id, reply_id)
-        replies = PluginStore.get(CannedReply::PLUGIN_NAME, CannedReply::STORE_NAME)
-        reply = replies[reply_id]
-        reply["usages"] ||= 0
-        reply["usages"] += 1
-        replies[reply_id] = reply
-        PluginStore.set(CannedReply::PLUGIN_NAME, CannedReply::STORE_NAME, replies)
-      end
-
-      def add_default_reply()
-        add(1, I18n.t("replies.default_reply.title"), I18n.t("replies.default_reply.body"))
+  DiscourseCannedReplies::Engine.routes.draw do
+    resources :canned_replies, path: "/", only: %i[index create destroy update] do
+      member do
+        get "reply"
+        patch "use"
       end
     end
   end
 
-  require_dependency "application_controller"
-
-  class CannedReply::CannedRepliesController < ::ApplicationController
-    requires_plugin CannedReply::PLUGIN_NAME
-
-    before_action :ensure_logged_in
-    before_action :ensure_canned_replies_enabled
-    skip_before_action :check_xhr
-
-    def ensure_canned_replies_enabled
-      guardian.ensure_can_use_canned_replies!
-    end
-
-    def create
-      guardian.ensure_can_edit_canned_replies!
-
-      title = params.require(:title)
-      content = params.require(:content)
-      user_id = current_user.id
-
-      record = CannedReply::Reply.add(user_id, title, content)
-      render json: record
-    end
-
-    def destroy
-      guardian.ensure_can_edit_canned_replies!
-
-      reply_id = params.require(:id)
-      user_id = current_user.id
-      record = CannedReply::Reply.remove(user_id, reply_id)
-
-      render json: record
-    end
-
-    def reply
-      reply_id = params.require(:id)
-      user_id = current_user.id
-
-      record = CannedReply::Reply.get_reply(user_id, reply_id)
-      render json: record
-    end
-
-    def update
-      guardian.ensure_can_edit_canned_replies!
-
-      reply_id = params.require(:id)
-      title = params.require(:title)
-      content = params.require(:content)
-      user_id = current_user.id
-
-      record = CannedReply::Reply.edit(user_id, reply_id, title, content)
-      render json: record
-    end
-
-    def use
-      reply_id = params.require(:id)
-      user_id = current_user.id
-      record = CannedReply::Reply.use(user_id, reply_id)
-
-      render json: record
-    end
-
-    def index
-      user_id = current_user.id
-      replies = CannedReply::Reply.all(user_id)
-
-      render json: { replies: replies }
-    end
+  Discourse::Application.routes.append do
+    mount ::DiscourseCannedReplies::Engine, at: "/canned_replies"
   end
 
   add_to_class(:user, :can_edit_canned_replies?) do
@@ -182,15 +62,4 @@ after_initialize do
   add_to_serializer(:current_user, :can_use_canned_replies) { object.can_use_canned_replies? }
 
   add_to_serializer(:current_user, :can_edit_canned_replies) { object.can_edit_canned_replies? }
-
-  CannedReply::Engine.routes.draw do
-    resources :canned_replies, path: "/", only: %i[index create destroy update] do
-      member do
-        get "reply"
-        patch "use"
-      end
-    end
-  end
-
-  Discourse::Application.routes.append { mount ::CannedReply::Engine, at: "/canned_replies" }
 end
