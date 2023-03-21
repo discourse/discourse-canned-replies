5a2d9a11ef3f07fc781acd83770bafc14eca2c1b
Bianca Nenciu <nbianca@users.noreply.github.com>
2023-03-21 14:08:40 +0200

    DEV: Update plugin to match latest guidelines (#121)
    This commit updates the plugin to follow the latest guidelines, as shown
    in discourse-plugin-skeleton, removing the explicit require calls and
    using config/routes.rb for defining all routes.

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+DiscourseCannedReplies::Engine.routes.draw do
+  resources :canned_replies, path: "/", only: %i[index create destroy update] do
+    member do
+      get "reply"
+      patch "use"
+    end
+  end
+end
+
+Discourse::Application.routes.append do
+  mount ::DiscourseCannedReplies::Engine, at: "/canned_replies"
+end
